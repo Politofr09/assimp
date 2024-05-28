770ab6298d3a25cb72c5e23d8055052d70c2a645
Politofr09 <deshidratatorpa@gmail.com>
2024-05-28 18:24:42 +0200

    update

--- premake5.lua
+++ premake5.lua
@@ -1,69 +1,142 @@
+project '*'
+	includedirs {
+		'_config_headers/',
+		'_config_headers/assimp/', -- Location of assimp's config.h, for a template see include/assimp/config.h.in
+		'assimp/include/',
+	}
+
+	filter "system:windows"
+		ignoredefaultlibraries { "libcmt.lib" }
+
 project "assimp"
-    location "assimp"
     kind "StaticLib"
     language "C++"
     cppdialect "C++17"
-    staticruntime "On"
 
     targetdir ("bin/%{prj.name}")
     objdir ("bin-obj/%{prj.name}")
 
-    files
-    {
-        "assimp/include/assimp/**.h",
-        "assimp/include/assimp/**.hpp",
-        "assimp/include/assimp/config.h",
-        "assimp/code/**.cpp",
-        "assimp/code/**.h",
-        "assimp/contrib/clipper/**.cpp",
-        "assimp/contrib/clipper/**.h",
-        "assimp/contrib/ConvertUTF/**.cpp",
-        "assimp/contrib/ConvertUTF/**.h",
-        "assimp/contrib/irrXML/**.cpp",
-        "assimp/contrib/irrXML/**.h",
-        "assimp/contrib/openddlparser/**.cpp",
-        "assimp/contrib/openddlparser/**.h",
-        "assimp/contrib/poly2tri/**.cc",
-        "assimp/contrib/poly2tri/**.h",
-        "assimp/contrib/zip/src/**.c",
-        "assimp/contrib/zip/src/**.h",
-    }
+	warnings 'Off'
+	optimize 'Speed'
 
-    includedirs
-    {
-        "assimp/include",
-        "assimp/code",
-        "assimp/contrib",
-        "assimp/contrib/irrXML",
-        "assimp/contrib/openddlparser/include",
-        "assimp/contrib/unzip",
-        "assimp/contrib/clipper",
-        "assimp/contrib/poly2tri",
-        "assimp/contrib/ConvertUTF",
-        "assimp/contrib/zlib",
-        "assimp/contrib/zip/src",
-        "assimp/contrib/rapidjson/include"
-    }
+	includedirs {
+		'include',
+		'_config_headers/',
+		'contrib/',
+		'contrib/irrXML/',
+		'contrib/unzip/',
+		'contrib/rapidjson/include/',
+		'contrib/pugixml/src/',
+		'contrib/zlib/',
+		'contrib/utf8cpp/source',
+		'code',
+        '../assimp'
+	}
+
+	files {
+		-- Dependencies
+		'contrib/unzip/**',
+		'contrib/irrXML/**',
+		'contrib/zlib/*',
+
+		-- Common
+		'code/Common/**',
+		'code/PostProcessing/**',
+		'code/Material/**',
+		'code/CApi/**',
+		'code/Geometry/**',
+
+		-- Importers
+		'code/AssetLib/IQM/**',
+		'code/AssetLib/Assbin/**',
+		'code/AssetLib/Collada/**',
+		'code/AssetLib/Obj/**',
+		'code/AssetLib/Blender/**', 'contrib/poly2tri/poly2tri/**',
+		'code/AssetLib/FBX/**',
+		-- 'code/glTF2/**',
+		-- 'code/glTF/**',
+		'code/Assbin/**' -- Very fast format to parse/write, useful for caching
+	}
+	-- Importers
+	defines {
+		'ASSIMP_BUILD_NO_3D_IMPORTER',
+		'ASSIMP_BUILD_NO_3DS_IMPORTER',
+		'ASSIMP_BUILD_NO_3MF_IMPORTER',
+		'ASSIMP_BUILD_NO_AC_IMPORTER',
+		'ASSIMP_BUILD_NO_AMF_IMPORTER',
+		'ASSIMP_BUILD_NO_ASE_IMPORTER',
+		-- 'ASSIMP_BUILD_NO_ASSBIN_IMPORTER'
+		'ASSIMP_BUILD_NO_B3D_IMPORTER',
+		-- 'ASSIMP_BUILD_NO_BLEND_IMPORTER',
+		'ASSIMP_BUILD_NO_BVH_IMPORTER',
+		'ASSIMP_BUILD_NO_C4D_IMPORTER',
+		'ASSIMP_BUILD_NO_COB_IMPORTER',
+		-- 'ASSIMP_BUILD_NO_COLLADA_IMPORTER',
+		'ASSIMP_BUILD_NO_CSM_IMPORTER',
+		'ASSIMP_BUILD_NO_DXF_IMPORTER',
+		-- 'ASSIMP_BUILD_NO_FBX_IMPORTER',
+		'ASSIMP_BUILD_NO_GLTF_IMPORTER',
+		'ASSIMP_BUILD_NO_HMP_IMPORTER',
+		'ASSIMP_BUILD_NO_IFC_IMPORTER',
+		'ASSIMP_BUILD_NO_IRR_IMPORTER',
+		'ASSIMP_BUILD_NO_IRRMESH_IMPORTER',
+		'ASSIMP_BUILD_NO_LWO_IMPORTER',
+		'ASSIMP_BUILD_NO_LWS_IMPORTER',
+		'ASSIMP_BUILD_NO_M3D_IMPORTER',
+		'ASSIMP_BUILD_NO_MD2_IMPORTER',
+		'ASSIMP_BUILD_NO_MD3_IMPORTER',
+		'ASSIMP_BUILD_NO_MD5_IMPORTER',
+		'ASSIMP_BUILD_NO_MDC_IMPORTER',
+		'ASSIMP_BUILD_NO_MDL_IMPORTER',
+		'ASSIMP_BUILD_NO_MMD_IMPORTER',
+		'ASSIMP_BUILD_NO_MS3D_IMPORTER',
+		'ASSIMP_BUILD_NO_NDO_IMPORTER',
+		'ASSIMP_BUILD_NO_NFF_IMPORTER',
+		-- 'ASSIMP_BUILD_NO_OBJ_IMPORTER',
+		'ASSIMP_BUILD_NO_OFF_IMPORTER',
+		'ASSIMP_BUILD_NO_OGRE_IMPORTER',
+		'ASSIMP_BUILD_NO_OPENGEX_IMPORTER',
+		'ASSIMP_BUILD_NO_PLY_IMPORTER',
+		'ASSIMP_BUILD_NO_Q3BSP_IMPORTER',
+		'ASSIMP_BUILD_NO_Q3D_IMPORTER',
+		'ASSIMP_BUILD_NO_RAW_IMPORTER',
+		'ASSIMP_BUILD_NO_SIB_IMPORTER',
+		'ASSIMP_BUILD_NO_SMD_IMPORTER',
+		'ASSIMP_BUILD_NO_STEP_IMPORTER',
+		'ASSIMP_BUILD_NO_STL_IMPORTER',
+		'ASSIMP_BUILD_NO_TERRAGEN_IMPORTER',
+		'ASSIMP_BUILD_NO_X_IMPORTER',
+		'ASSIMP_BUILD_NO_X3D_IMPORTER',
+		'ASSIMP_BUILD_NO_XGL_IMPORTER'
+	}
+	-- Exporters
+	defines {
+		'ASSIMP_BUILD_NO_COLLADA_EXPORTER',
+		'ASSIMP_BUILD_NO_X_EXPORTER',
+		'ASSIMP_BUILD_NO_STEP_EXPORTER',
+		'ASSIMP_BUILD_NO_OBJ_EXPORTER',
+		'ASSIMP_BUILD_NO_STL_EXPORTER',
+		'ASSIMP_BUILD_NO_PLY_EXPORTER',
+		'ASSIMP_BUILD_NO_3DS_EXPORTER',
+		'ASSIMP_BUILD_NO_GLTF_EXPORTER',
+		-- 'ASSIMP_BUILD_NO_ASSBIN_EXPORTER',
+		'ASSIMP_BUILD_NO_ASSXML_EXPORTER',
+		'ASSIMP_BUILD_NO_X3D_EXPORTER',
+		'ASSIMP_BUILD_NO_FBX_EXPORTER',
+		'ASSIMP_BUILD_NO_M3D_EXPORTER',
+		'ASSIMP_BUILD_NO_3MF_EXPORTER',
+		'ASSIMP_BUILD_NO_ASSJSON_EXPORTER'
+	}
 
     filter "system:windows"
-        systemversion "latest"
+        defines '_CRT_SECURE_NO_WARNINGS'
 
-        defines
-        {
-            "_CRT_SECURE_NO_WARNINGS"
-        }
-
-    filter "configurations:Debug"
-        defines "DEBUG"
-        runtime "Debug"
+	filter "configurations:Debug"
         symbols "On"
+        optimize "Off"  -- Turn off optimization for debug builds
+		runtime "Debug"
 
     filter "configurations:Release"
-        defines "RELEASE"
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "DIST"
-        runtime "Release"
-        optimize "On"
+        symbols "Off"
+        optimize "Full"  -- Full optimization for release builds
+		runtime "Release"
