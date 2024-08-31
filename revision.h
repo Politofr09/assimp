20025255f1f993937a0b974a2c16248f6c614ac7
Politofr09 <deshidratatorpa@gmail.com>
2024-08-31 11:33:03 +0200

    Added config files manually

---
+++ revision.h
@@ -0,0 +1,15 @@
+#ifndef REVISION_H_INC
+#define REVISION_H_INC
+
+// Git commit version, representing the current revision of the code.
+#define GitVersion 0xd994b84 // Replace with actual commit hash or version code
+
+// Git branch name, representing the branch of the current code.
+#define GitBranch "main" // Replace with actual branch name
+
+// Versioning for Assimp
+#define VER_MAJOR 5  // Major version of Assimp
+#define VER_MINOR 2  // Minor version of Assimp
+#define VER_PATCH 0  // Patch version of Assimp
+
+#endif // REVISION_H_INC
